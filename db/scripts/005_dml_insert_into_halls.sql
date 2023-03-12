919c54804b258df3c8d2f7f6f5b5438ffb4642b5
mihanchik94 <mihanchik94@gmail.com>
2023-03-12 22:18:12 +0300

    Add scripts for database

---
+++ db/scripts/005_dml_insert_into_halls.sql
@@ -0,0 +1,5 @@
+insert into halls (name, row_count, place_count, description) values ('Зал 1', 5, 20, 'Зал №1');
+insert into halls (name, row_count, place_count, description) values ('Зал 2', 5, 20, 'Зал №2');
+insert into halls (name, row_count, place_count, description) values ('Зал 3', 5, 20, 'Зал №3');
+insert into halls (name, row_count, place_count, description) values ('Зал 4', 5, 20, 'Зал №4');
+insert into halls (name, row_count, place_count, description) values ('Зал 5', 5, 20, 'Зал №5');
